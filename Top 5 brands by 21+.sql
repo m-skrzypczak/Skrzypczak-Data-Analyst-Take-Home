f1e520bf5c7582f259096a8ff7e3b7957ecf98f4
m-skrzypczak <michael.j.skrzypczak@gmail.com>
2025-02-03 20:15:05 -0600

    Add files via upload

---
+++ Top 5 brands by 21+.sql
@@ -0,0 +1,10 @@
+--What are the top 5 brands by receipts scanned among users 21 and over?
+SELECT TOP 5 b.Brand, -- Limiting Query to top 5 brands
+    COUNT(DISTINCT a.RECEIPT_ID) as Receipts_Scanned -- Utilizing DISTINCT to ensure each receipt is only counted once
+FROM Transactions a
+	LEFT JOIN products b ON a.barcode=b.barcode
+	LEFT JOIN Users c ON a.user_id=c.id
+WHERE a.barcode IS NOT NULL AND b.brand IS NOT NULL -- Excluding receipts without barcodes and barcodes without brands
+	AND DATEDIFF(year,c.birth_date,GETDATE())>=21 -- Limiting to users 21+
+GROUP BY b.brand
+ORDER BY receipts_scanned DESC -- Ordering from smallest to largest
